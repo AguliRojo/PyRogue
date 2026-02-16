fd9ab534a7e6f0185a10108dbad757110e64bcd4
Aguli <tomspakovsky@gmail.com>
2026-02-17 00:32:03 +0100

    Cleaned code. added collision with enemy Map has out of bound issues. There is a bad reference to map/hero/location. Duplication was not addressed

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 import random
 
 # Player default values
-base_player_health = 50
-base_player_attack = 5
-default_name = "Adam"
-
+class player_data:
+    base_player_health = 50
+    base_player_attack = 5
+    default_name = "Adam"
 
 class Player:
-    def __init__(self, name=default_name, health=base_player_health, attack=base_player_attack):
+    def __init__(self, name=player_data.default_name, health=player_data.base_player_health, attack=player_data.base_player_attack):
         self.name = name
         self.health = health
         self.attack = attack
@@ -68,19 +68,14 @@ def display_map():
     return world_map
 
 
-Your_name = input("What is your name traveler?")
+
 # Initialize a 5x6 grid with independent values.
-world_map_dimensions = [[MapTiles.floor_tile] * 5 for _ in range(6)]
+sizeof_map_x = 5
+sizeof_map_y = 6
+world_map_dimensions = [[MapTiles.floor_tile] * sizeof_map_x for _ in range(sizeof_map_y)]
 hero_coordinates = [random.randint(0, len(world_map_dimensions) - 1),
                         random.randint(0, len(world_map_dimensions[0]) - 1)]
 enemy_coordinates = generate_enemy()
-
-def action_table():
-    """moving of entities, calling functions for utility"""
-    north=[-1,0]
-    west=[0,-1]
-    east=[0,1]
-    south=[1,0]
 
 def move_table():
     """Tables used for alternative button"""
@@ -94,28 +89,76 @@ def move_table():
     return moveset
 
 def move_action(action):
-    hero_location = world_map_dimensions[hero_coordinates[0]][hero_coordinates[1]]
-    print(f'{MapTiles.hero} - hero location\n{hero_coordinates[0]+1} - x\n{hero_coordinates[1]+1} - y')
+    # hero_location = world_map_dimensions[hero_coordinates[0]][hero_coordinates[1]]
+    # print(f'{MapTiles.hero} - hero location\n{hero_coordinates[0]+1} - x\n{hero_coordinates[1]+1} - y')
     if action in move_table():
-        print(move_table())
+        # print(move_table())
         dx, dy = move_table()[action]
+        print(f'Before move:\nx: {hero_coordinates[1]}, y:{hero_coordinates[0]}')
         hero_coordinates[0] += dx
         hero_coordinates[1] += dy
-        if hero_coordinates[0] < 0 or hero_coordinates[0] > len(display_map()[0]):
+        print(f'After move:\nx: {hero_coordinates[1]}, y:{hero_coordinates[0]}')
+        # if hit a wall return message and don't change positions
+        # TODO: display_map() returns map generation, and doesnt have rules so you can call -1 x axis
+        #       But you can't call out of bounds so bigger/less than -+len() crashes
+        if hero_coordinates[0] < 0 or hero_coordinates[0] > sizeof_map_y:
+            # y lock
             hero_coordinates[0] -= dx
-            hero_coordinates[1] -= -dy
+            hero_coordinates[1] -= dy
             print("Invalid move, you've hit a wall")
-        if hero_coordinates[1] < 0 or hero_coordinates[1] > len(display_map()[1]):
+        if hero_coordinates[1] < 0 or hero_coordinates[1] > sizeof_map_x:
+            # x lock
             hero_coordinates[0] -= dx
-            hero_coordinates[1] -= -dy
+            hero_coordinates[1] -= dy
             print("Invalid move, you've hit a wall")
-        print(f'{MapTiles.hero} - hero location\n{hero_coordinates[0]+1} - x\n{hero_coordinates[1]+1} - y')
+        # if hit an enemy dont move, but interact
+        if hero_coordinates[0] == enemy_coordinates[0] and hero_coordinates[1] == enemy_coordinates[1]:
+            hero_coordinates[0] -= dx
+            hero_coordinates[1] -= dy
+            print(Colors.RED + "Punch!" + Colors.RESET)
+        # print(f'{MapTiles.hero} - hero location\n{hero_coordinates[0]+1} - x\n{hero_coordinates[1]+1} - y')
     else:
         print("Invalid move")
 
-def game_loop(your_name):
+
+# def move_action(action):
+#     # hero_location = world_map_dimensions[hero_coordinates[0]][hero_coordinates[1]]
+#     # print(f'{MapTiles.hero} - hero location\n{hero_coordinates[0]+1} - x\n{hero_coordinates[1]+1} - y')
+#     if action in move_table():
+#         # print(move_table())
+#         dx, dy = move_table()[action]
+#         # if hit a wall return message and don't change positions
+#         # TODO: display_map() returns map generation, and doesnt have rules so you can call -1 x axis
+#         #       But you can't call out of bounds so bigger/less than -+len() crashes
+#         if (hero_coordinates[0]+dx) < 0 or hero_coordinates[0]+dx > sizeof_map_y:
+#             # y lock
+#             hero_coordinates[0] -= dx
+#             hero_coordinates[1] -= dy
+#             print("Invalid move, you've hit a wall")
+#         if hero_coordinates[1]+dy < 0 or hero_coordinates[1]+dy > sizeof_map_x:
+#             # x lock
+#             hero_coordinates[0] -= dx
+#             hero_coordinates[1] -= dy
+#             print("Invalid move, you've hit a wall")
+#         # if hit an enemy dont move, but interact
+#         if hero_coordinates[0] == enemy_coordinates[0]+dx and hero_coordinates[1]+dy == enemy_coordinates[1]:
+#             hero_coordinates[0] -= dx
+#             hero_coordinates[1] -= dy
+#             print(Colors.RED + "Punch!" + Colors.RESET)
+#         else:
+#             print(f'Before move:\nx: {hero_coordinates[1]}, y:{hero_coordinates[0]}')
+#             hero_coordinates[0] += dx
+#             hero_coordinates[1] += dy
+#             print(f'After move:\nx: {hero_coordinates[1]}, y:{hero_coordinates[0]}')
+#         # print(f'{MapTiles.hero} - hero location\n{hero_coordinates[0]+1} - x\n{hero_coordinates[1]+1} - y')
+#     else:
+#         print("Invalid move")
+
+
+def game_loop():
+    your_name = input("What is your name traveler?")
     if your_name == "":
-        your_name = "Adam"
+        your_name = player_data.default_name
     you = Player(name=your_name)
     print(you.your_name())
     while True:
@@ -124,4 +167,4 @@ def game_loop(your_name):
         move_action(action)
         # if action == "q":
         #     return False
-game_loop(Your_name)
+game_loop()
